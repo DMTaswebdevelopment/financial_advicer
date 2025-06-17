38281c441637be2437fe96eebbd1c6e16058f87d
DMTaswebdevelopment <webdevelopment@dmtas.com.au>
2025-06-16 18:05:26 -0800

    test

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,6 +3,7 @@
 
 import LandingPage from "./landingpage/page";
 import Footer from "@/component/ui/footer/Footer";
+import PublicRoute from "@/components/routes/PublicRoutes/PublicRoute";
 import { usePathname } from "next/navigation";
 
 export default function UserHome() {
@@ -11,11 +12,11 @@ export default function UserHome() {
   const hideFooter = pathname === "/searchresult";
 
   return (
-    <>
+    <PublicRoute>
       <LandingPage />
 
       {!hideFooter && <Footer />}
-    </>
+    </PublicRoute>
 
     // <LandingPage />
   );
