dcb337a07440c2075cc1ba5d95f94e11531e0173
DMTaswebdevelopment <webdevelopment@dmtas.com.au>
2025-06-24 18:47:08 -0800

    fix

--- app/(user)/landingpage/page.tsx
+++ app/(user)/landingpage/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import CatchSection from "@/component/ui/section/CatchSection/CatchSection";
-import HelpfulQuestionSection from "@/component/ui/section/HelpfulQuestionSection/HelpfulQuestionSection";
+// import HelpfulQuestionSection from "@/component/ui/section/HelpfulQuestionSection/HelpfulQuestionSection";
 import Herosection from "@/component/ui/section/herosection/Herosection";
 import MissionSection from "@/component/ui/section/missionsection/MissionSection";
 import PricingSection from "@/component/ui/section/PricingSection/PricingSection";
